63ff68741027ded07d803ad1e5dc1de49d28c798
Alessandro Lenzi <alessandro@orra.com.br>
2023-01-18 18:25:52 +0044

    feat: add zod with userSchema and type infering

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-interface User {
-  name: string;
-  age: number;
-}
+import { z } from 'zod';
+
+const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'Name must be at least 3 characters long.' })
+    .transform((name) => name.toLocaleUpperCase()),
+  age: z.number().min(18, { message: 'Age must be at least 18.' }),
+});
+
+type User = z.infer<typeof userSchema>;
 
 function saveUserToDatabase(user: User) {
+  const { name, age } = userSchema.parse(user);
   // save to DB
-  console.log('Saving', user);
+  console.log('Saving', { name, age });
 }
 
 saveUserToDatabase({
